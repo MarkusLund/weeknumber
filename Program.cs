db5bb34383b50810c2684f93477efa66e50bcc33
MarkusLund <markuslund92@gmail.com>
2018-03-18 22:41:58 +0100

    Outputs current weeknumber to console

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace weeknumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weekNumber = GetIso8601WeekOfYear(DateTime.Now).ToString();
+            Console.WriteLine($"Current week number: {weekNumber}");
+        }
+
+        // Code written by il_guru (https://stackoverflow.com/questions/11154673/get-the-correct-week-number-of-a-given-date)
+        public static int GetIso8601WeekOfYear(DateTime time)
+        {
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        }
+    }
+}
